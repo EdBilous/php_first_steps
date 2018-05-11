3864febf8bbbe180e10b308e10edbd632f7d0f32
mnazon <e.v.bilous@gmail.com>
2018-05-11 07:23:15 +0300

    07. 05

--- hm_wk/9 7.05/mysite.zz/functions.php
+++ hm_wk/9 7.05/mysite.zz/functions.php
@@ -155,43 +155,6 @@ function search($query)
     $query = trim($query); 
     $query = htmlspecialchars($query);
 
-    // if (!empty($query)) 
-    // { 
-    //     if (strlen($query) < 3) {
-    //         $text = '<p>Слишком короткий поисковый запрос.</p>';
-    //     } else if (strlen($query) > 128) {
-    //         $text = '<p>Слишком длинный поисковый запрос.</p>';
-    //     } else { 
-    //         $result = getSearchArticle($query)
-
-    //         // if ($result > 0) { 
-    //         //     $row = mysql_fetch_assoc($result); 
-    //         //     $num = mysql_num_rows($result);
-
-    //         //     $text = '<p>По запросу <b>'.$query.'</b> найдено совпадений: '.$num.'</p>';
-
-    //         //     do {
-    //         //         // Делаем запрос, получающий ссылки на статьи
-    //         //         $q1 = "SELECT `link` FROM `table_name` WHERE `uniq_id` = '$row[page_id]'";
-    //         //         $result1 = mysql_query($q1);
-
-    //         //         if (mysql_affected_rows() > 0) {
-    //         //             $row1 = mysql_fetch_assoc($result1);
-    //         //         }
-
-    //         //         $text .= '<p><a> href="'.$row1['link'].'/'.$row['category'].'/'.$row['uniq_id'].'" title="'.$row['title_link'].'">'.$row['title'].'</a></p>
-    //         //         <p>'.$row['desc'].'</p>';
-
-    //         //     } while ($row = mysql_fetch_assoc($result)); 
-    //         // } else {
-    //         //     $text = '<p>По вашему запросу ничего не найдено.</p>';
-    //         // }
-    //         } 
-    //     } else {
-    //         $text = '<p>Задан пустой поисковый запрос.</p>';
-    //     }
-    //     return $result; 
-    // }
 
     if (!empty($query)) {
         if (strlen($query) < 3) {
